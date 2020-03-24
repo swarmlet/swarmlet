04b1d525cbe0806ff2ae279cc218eadd6b8f24aa
Tjerk Woudsma <tjerkwoudsma@gmail.com>
2020-03-24 04:23:39 +0100

    add python-redis example

---
+++ examples/python-redis/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from redis import Redis
+import socket
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    return '<h1>Hello World!</h1>I have been seen {} times<br>HostName = {}<br>IP = {}<br>Try refreshing the page.'.format(count,
+        host_name, 
+        host_ip)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
